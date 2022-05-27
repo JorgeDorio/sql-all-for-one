3e77fc60dc5abf03271c765270987174ca87d9ac
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2022-05-26 21:42:18 -0300

    fexing querys

--- desafio19.sql
+++ desafio19.sql
@@ -1,1 +1,1 @@
-SELECT COUNT(employee_id) FROM northwind.orders o WHERE employee_id IN (5,6) AND shipper_id = 2;
+SELECT COUNT(employee_id) AS orders_count FROM northwind.orders o WHERE employee_id IN (5,6) AND shipper_id = 2;
